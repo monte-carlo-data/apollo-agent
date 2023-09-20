528a00329c1233340c8986985b697410827d9a76
Martin Rostan <mrostan@montecarlodata.com>
2023-09-20 09:40:20 -0300

    fix stacktrace generation, it was returning the current stack instead of the exception stack

--- apollo/agent/utils.py
+++ apollo/agent/utils.py
@@ -10,7 +10,7 @@ class AgentUtils:
         error = " ".join(traceback.format_exception_only(last_type, last_value))
         if prefix:
             error = f"{prefix} {error}"
-        stack_trace = traceback.format_stack()
+        stack_trace = traceback.format_tb(last_value.__traceback__)
         return cls.response_for_error(error, stack_trace)
 
     @staticmethod
