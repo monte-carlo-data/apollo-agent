cdc187b32454c7369be457dad38704cefb25ce72
Martin Rostan <mrostan@montecarlodata.com>
2023-09-21 08:41:33 -0300

    added a dictionary with the factory methods instead of a potential long list of chained "ifs"

--- apollo/agent/proxy_client_factory.py
+++ apollo/agent/proxy_client_factory.py
@@ -4,22 +4,29 @@ from apollo.agent.models import AgentError
 from apollo.integrations.base_proxy_client import BaseProxyClient
 
 
+def _get_proxy_client_bigquery(credentials: Optional[Dict]) -> BaseProxyClient:
+    # import driver modules only when needed
+    # in subsequent versions we might not want to bundle all dependencies in a single image
+    from apollo.integrations.bigquery.bq_proxy_client import BqProxyClient
+
+    return BqProxyClient(credentials=credentials)
+
+
+_CLIENT_FACTORY_MAPPING = {
+    "bigquery": _get_proxy_client_bigquery,
+}
+
+
 class ProxyClientFactory:
+
     @classmethod
     def get_proxy_client(
         cls, connection_type: str, credentials: Dict
     ) -> BaseProxyClient:
-        if connection_type == "bigquery":
-            return cls._get_proxy_client_bigquery(credentials)
+        factory_method = _CLIENT_FACTORY_MAPPING.get(connection_type)
+        if factory_method:
+            return factory_method(credentials)
         else:
             raise AgentError(
                 f"Connection type not supported by this agent: {connection_type}"
             )
-
-    @staticmethod
-    def _get_proxy_client_bigquery(credentials: Optional[Dict]) -> BaseProxyClient:
-        # import driver modules only when needed
-        # in subsequent versions we might not want to bundle all dependencies in a single image
-        from apollo.integrations.bigquery.bq_proxy_client import BqProxyClient
-
-        return BqProxyClient(credentials=credentials)
