573d699c4ec7c1d5acd70d1c249016a854ccd3f7
Martin Rostan <mrostan@montecarlodata.com>
2023-09-25 21:14:45 -0300

    added endpoint to test storage listing

--- apollo/interfaces/generic/main.py
+++ apollo/interfaces/generic/main.py
@@ -1,6 +1,10 @@
 import logging
+import os
+import sys
 from typing import Optional, Dict, Tuple
 
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
 from flask import Flask, request
 
 from apollo.agent.agent import Agent
@@ -35,6 +39,26 @@ def agent_execute(
         connection_type, operation_name, operation, credentials
     )
     return response.result, response.status_code
+
+
+@app.route("/hello/<name>", methods=["GET"])
+def hello(name: str):
+    return f"hello {name} from docker 19 {os.getenv('PYTHON_VERSION')} {sys.version}\n"
+
+
+@app.route("/list", methods=["GET"])
+def list_files():
+    # connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+    # blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    account_url = "https://mcddevagenttest.blob.core.windows.net"
+    default_credential = DefaultAzureCredential()
+
+    # Create the BlobServiceClient object
+    blob_service_client = BlobServiceClient(account_url, credential=default_credential)
+    container_name = "test"
+    container_client = blob_service_client.get_container_client(container_name)
+    blobs = container_client.list_blobs()
+    return {"blobs": [blob.name for blob in blobs]}
 
 
 if __name__ == "__main__":
