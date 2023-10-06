75ed2670f00fae108e23947d5114bc585e84fe91
Martin Rostan <mrostan@montecarlodata.com>
2023-10-06 14:32:49 -0300

    fixed description in setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name="mcd-agent",
     use_scm_version=True,
     license="Apache Software License (Apache 2.0)",
-    description="Monte Carlo's Python SDK",
+    description="Monte Carlo's Agent",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     author="Monte Carlo Data, Inc",
