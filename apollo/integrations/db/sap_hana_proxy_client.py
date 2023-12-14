d03dd1069b7c6c9e3cc342af5303e059c302cfb8
ssmith <ssmith@montecarlodata.com>
2023-12-14 16:17:32 -0600

    add sap hana proxy client

---
+++ apollo/integrations/db/sap_hana_proxy_client.py
@@ -0,0 +1,26 @@
+from typing import Optional, Dict, Any
+
+from hdbcli import dbapi
+
+from apollo.integrations.db.base_db_proxy_client import BaseDbProxyClient
+
+_ATTR_CONNECT_ARGS = "connect_args"
+
+
+class SAPHanaProxyClient(BaseDbProxyClient):
+    """
+    Proxy client for SAP Hana. Credentials are expected to be supplied under "connect_args" and
+    will be passed directly to `dbapi.connect`, so only attributes supported as parameters by
+    `dbapi.connect` should be passed.
+    """
+
+    def __init__(self, credentials: Optional[Dict], **kwargs: Any):
+        if not credentials or _ATTR_CONNECT_ARGS not in credentials:
+            raise ValueError(
+                f"SAP HANA database agent client requires {_ATTR_CONNECT_ARGS} in credentials"
+            )
+        self._connection = dbapi.connect(**credentials[_ATTR_CONNECT_ARGS])  # type: ignore
+
+    @property
+    def wrapped_client(self):
+        return self._connection
