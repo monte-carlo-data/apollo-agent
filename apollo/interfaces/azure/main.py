18b068bd4377141076700c981f713aaaa38c1f15
Martin Rostan <mrostan@montecarlodata.com>
2023-12-22 15:10:51 -0300

    forcing an error to test the flask exception handler

--- apollo/interfaces/azure/main.py
+++ apollo/interfaces/azure/main.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Tuple, Dict, Optional
 
-from flask import request
+# from flask import request
 from werkzeug.exceptions import InternalServerError
 
 from apollo.agent.utils import AgentUtils
