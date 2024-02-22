9142baa6f1a759a0bfe01e7b1094cec7c5f1cba7
Santiago Aguiar <saguiar@montecarlodata.com>
2024-02-22 11:14:56 -0300

    add module support

--- tests/test_agent_scripts.py
+++ tests/test_agent_scripts.py
@@ -9,7 +9,7 @@ from apollo.agent.models import AgentCommands, AgentScript
 from tests.sample_proxy_client import SampleProxyClient
 
 
-class AgentCommandsTests(TestCase):
+class AgentScriptsTests(TestCase):
     def setUp(self) -> None:
         self._query = "SELECT * FROM table"
         self._client = SampleProxyClient()
@@ -22,7 +22,11 @@ class AgentCommandsTests(TestCase):
                 {
                     "operation_name": "test",
                     "trace_id": "1",
-                    "script": """
+                    "entry_module": "main",
+                    "modules": [
+                        {
+                            "name": "main",
+                            "source": """
 def execute_script_handler(client, context, sql_query):        
     if client is None:
         raise Exception('is none')    
@@ -30,6 +34,8 @@ def execute_script_handler(client, context, sql_query):
         cursor.execute(sql_query)
         return {'rows': cursor.fetchmany(10)}
 """,
+                        }
+                    ],
                     "kwargs": {
                         "sql_query": self._query,
                     },
@@ -41,7 +47,7 @@ def execute_script_handler(client, context, sql_query):
         expected_result = {"rows": cursor.fetchmany()}
         self.assertEqual(expected_result, result)
 
-    def test_use_import_fails(self):
+    def test_use_import_fails_for_unexisting_module(self):
         result = Agent(LoggingUtils())._execute_script(
             self._client,
             "test",
@@ -49,16 +55,69 @@ def execute_script_handler(client, context, sql_query):
                 {
                     "operation_name": "test",
                     "trace_id": "1",
-                    "script": """
+                    "entry_module": "main",
+                    "modules": [
+                        {
+                            "name": "main",
+                            "source": """
 import os
 def execute_script_handler(client, context, sql_query):                
     return os.getcwd()
     """,
+                        }
+                    ],
                     "kwargs": {},
                 }
             ),
         )
-        self.assertEqual(result[ATTRIBUTE_NAME_ERROR], "__import__ not found")
+        self.assertEqual(
+            result[ATTRIBUTE_NAME_ERROR], "Module 'os' not found in script"
+        )
+
+    def test_use_import_fails_for_existing_module(self):
+        result = Agent(LoggingUtils())._execute_script(
+            self._client,
+            "test",
+            AgentScript.from_dict(
+                {
+                    "operation_name": "test",
+                    "trace_id": "1",
+                    "entry_module": "main",
+                    "modules": [
+                        {
+                            "name": "helpers_foo",
+                            "source": """
+import helpers_bar                
+def foobar():                
+    return 'foo' + helpers_bar.bar()
+""",
+                        },
+                        {
+                            "name": "helpers_bar",
+                            "source": """                
+def bar():                
+    return "bar"
+""",
+                        },
+                        {
+                            "name": "main",
+                            "source": """
+import helpers_foo
+from helpers_foo import foobar as foobar2   
+def bar():
+    return "bar"
+def execute_script_handler(client, context, sql_query):                    
+    return f'{bar()}_{helpers_foo.foobar()}_{foobar2()}'
+        """,
+                        },
+                    ],
+                    "kwargs": {
+                        "sql_query": self._query,
+                    },
+                }
+            ),
+        )
+        self.assertEqual(result, "bar_foobar_foobar")
 
     def test_use_exec_fails(self):
         result = Agent(LoggingUtils())._execute_script(
@@ -68,9 +127,15 @@ def execute_script_handler(client, context, sql_query):
                 {
                     "operation_name": "test",
                     "trace_id": "1",
-                    "script": """
+                    "entry_module": "main",
+                    "modules": [
+                        {
+                            "name": "main",
+                            "source": """
 exec("return 1")
-        """,
+            """,
+                        }
+                    ],
                     "kwargs": {},
                 }
             ),
@@ -87,7 +152,11 @@ exec("return 1")
                 {
                     "operation_name": "test",
                     "trace_id": "1",
-                    "script": """
+                    "entry_module": "main",
+                    "modules": [
+                        {
+                            "name": "main",
+                            "source": """
 def some_func(client, context, sql_query):        
     if client is None:
         raise Exception('is none')    
@@ -95,6 +164,8 @@ def some_func(client, context, sql_query):
         cursor.execute(sql_query)
         return {'rows': cursor.fetchmany(10)}
 """,
+                        }
+                    ],
                     "kwargs": {
                         "sql_query": self._query,
                     },
@@ -102,5 +173,6 @@ def some_func(client, context, sql_query):
             ),
         )
         self.assertEqual(
-            result[ATTRIBUTE_NAME_ERROR], "execute_script_handler not found"
+            result[ATTRIBUTE_NAME_ERROR],
+            "'execute_script_handler' function not found in agent script",
         )
