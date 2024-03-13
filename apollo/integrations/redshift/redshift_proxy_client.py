58a7d3aa2f0e13172cb009c0e868ad1963cee173
Martin Rostan <mrostan@montecarlodata.com>
2024-03-13 18:19:09 -0300

    added new method to the Redshift proxy client: disable_decoding_in_driver that forces strings to be returned as bytes

--- apollo/integrations/redshift/redshift_proxy_client.py
+++ apollo/integrations/redshift/redshift_proxy_client.py
@@ -1,5 +1,5 @@
 from typing import Dict, Optional
-
+from psycopg2.extensions import register_type, BYTES, BYTESARRAY
 from apollo.integrations.db.postgres_proxy_client import PostgresProxyClient
 
 _ATTR_CONNECT_ARGS = "connect_args"
@@ -19,3 +19,11 @@ class RedshiftProxyClient(PostgresProxyClient):
         )
         if credentials and credentials.get("autocommit", False):
             self._connection.autocommit = True
+
+    def disable_decoding_in_driver(self):
+        """
+        Used when Redshift tables have mixed or unusual encodings. The effect is that string
+        columns are returned as bytes, so decoding needs to happen in custom code instead of psycopg2.
+        """
+        register_type(BYTES, self._connection)
+        register_type(BYTESARRAY, self._connection)
