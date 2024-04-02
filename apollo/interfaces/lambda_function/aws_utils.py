f09db912766723730520ef9a78652fd20ac8d829
Martin Rostan <mrostan@montecarlodata.com>
2024-04-02 09:36:13 -0300

    configured a 10 seconds timeout with a single retry when getting the current image as part of the health endpoint

---
+++ apollo/interfaces/lambda_function/aws_utils.py
@@ -0,0 +1,16 @@
+from botocore.config import Config
+
+
+def get_retrieve_current_image_boto_config() -> Config:
+    """
+    Configured the boto3 client (lambda or cloudformation) used to retrieve the current image
+    with 10 seconds timeout and a single retry, so it would take 20 seconds to fail instead
+    of 5 minutes (with the default settings).
+    """
+    return Config(
+        connect_timeout=10,
+        retries=dict(
+            mode="standard",
+            max_attempts=1,
+        ),
+    )
