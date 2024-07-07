497fbe18d282af452f45e597cce334053230bed1
Gilad Grushka <ggrushka@montecarlodata.com>
2024-07-07 10:24:04 +0300

    Merge from ggrushka/DAR-391

--- apollo/integrations/tableau/tableau_proxy_client.py
+++ apollo/integrations/tableau/tableau_proxy_client.py
@@ -139,10 +139,11 @@ class TableauProxyClient(BaseProxyClient):
             "Content-Type": content_type,
         }
         if path.startswith("/"):
-            url = f"{self._server.baseurl}{path}"
+            # Only https://example.com (without /api/version)
+            url = f"{self._server.server_address}{path}"
         else:
             url = f"{self._server.baseurl}/sites/{self._server.site_id}/{path}"
-        logger.info(f"TB used URL: {url}")
+        logger.info(f"Tableau url: {url}")
         response = requests.request(
             method=request_method, url=url, data=data, headers=headers, params=params
         )
