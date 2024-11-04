0e11e9f0c6f6b64e14397e32018e296642b3174c
ssmith <ssmith@montecarlodata.com>
2024-11-04 11:08:41 -0600

    fix hive proxy cursor method

--- apollo/integrations/db/hive_proxy_client.py
+++ apollo/integrations/db/hive_proxy_client.py
@@ -5,24 +5,10 @@ from typing import (
 )
 
 from impala import dbapi
-from impala.hiveserver2 import HiveServer2Connection
 
 from apollo.integrations.db.base_db_proxy_client import BaseDbProxyClient
 
 _ATTR_CONNECT_ARGS = "connect_args"
-
-
-class HiveProxyConnection(HiveServer2Connection):
-    def cursor(self):
-        # If close_finished_queries is true, impala will close every query once a DDL/DML query execution is finished
-        # or all rows are fetched. It will also call GetLog() before closing the query to get query metadata from Hive.
-        # GetLog() is not available for spark databricks causing this to break.
-        #
-        # Setting close_finished_queries to false  will only close queries when execute() is called again
-        # or the cursor is closed. GetLog() is not automatically called so spark databricks works.
-        # With False the cursor will not have a rowcount for DML statements, this is fine for MC.
-        # https://github.com/cloudera/impyla/blob/e4c76169f7e5765c09b11c92fceb862dbb9b72be/impala/hiveserver2.py#L122
-        return super().cursor(self, close_finished_queries=False)
 
 
 class HiveProxyClient(BaseDbProxyClient):
@@ -39,9 +25,18 @@ class HiveProxyClient(BaseDbProxyClient):
                 f"Hive agent client requires {_ATTR_CONNECT_ARGS} in credentials"
             )
 
-        self._connection = HiveProxyConnection(
-            dbapi.connect(**credentials[_ATTR_CONNECT_ARGS])
-        )
+        self._connection = dbapi.connect(**credentials[_ATTR_CONNECT_ARGS])
+
+    def cursor(self):
+        # If close_finished_queries is true, impala will close every query once a DDL/DML query execution is finished
+        # or all rows are fetched. It will also call GetLog() before closing the query to get query metadata from Hive.
+        # GetLog() is not available for spark databricks causing this to break.
+        #
+        # Setting close_finished_queries to false  will only close queries when execute() is called again
+        # or the cursor is closed. GetLog() is not automatically called so spark databricks works.
+        # With False the cursor will not have a rowcount for DML statements, this is fine for MC.
+        # https://github.com/cloudera/impyla/blob/e4c76169f7e5765c09b11c92fceb862dbb9b72be/impala/hiveserver2.py#L122
+        return self._connection.cursor(close_finished_queries=False)
 
     @property
     def wrapped_client(self):
