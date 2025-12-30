c27b8055b7f4d8a94583c5b774919863cd7f0923
Martin Rostan <mrostan@montecarlodata.com>
2025-12-30 17:12:38 -0300

    support getting subscription_id from default credentials, this is required to use the managed identity to access the storage account

--- apollo/integrations/azure_blob/utils.py
+++ apollo/integrations/azure_blob/utils.py
@@ -1,8 +1,13 @@
+import logging
 import os
+from typing import Optional
 
 from azure.identity import DefaultAzureCredential
 
 from apollo.common.agent.models import AgentConfigurationError
+from azure.mgmt.resource import SubscriptionClient
+
+logger = logging.getLogger(__name__)
 
 
 class AzureUtils:
@@ -30,15 +35,31 @@ class AzureUtils:
             raise AgentConfigurationError("Unable to get function name")
         return function_name
 
-    @staticmethod
-    def get_subscription_id() -> str:
+    @classmethod
+    def get_subscription_id(cls) -> str:
         owner_name = os.getenv(
             "WEBSITE_OWNER_NAME"
         )  # subscription_id+resource_group_region_etc
         if not owner_name:
+            # try getting the subscription id using the default credentials
+            subscription_id = cls._get_subscription_id_from_credentials()
+            if subscription_id:
+                return subscription_id
             raise AgentConfigurationError("Unable to get subscription id")
         if "+" not in owner_name:
             raise AgentConfigurationError(
                 f"Unable to get subscription id, invalid owner name: {owner_name}"
             )
         return owner_name.split("+")[0]
+
+    @staticmethod
+    def _get_subscription_id_from_credentials() -> Optional[str]:
+        client = SubscriptionClient(AzureUtils.get_default_credential())
+        subscriptions = list(client.subscriptions.list())
+        if not subscriptions:
+            logger.error("No subscriptions found using credentials")
+            return None
+        if len(subscriptions) > 1:
+            logger.error("Multiple subscriptions found using credentials")
+            return None
+        return subscriptions[0].subscription_id
