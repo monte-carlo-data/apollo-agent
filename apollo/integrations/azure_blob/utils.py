6b435365fd716eeea1e80b3ee490f97a1df2ba8e
Martin Rostan <mrostan@montecarlodata.com>
2025-12-30 17:55:56 -0300

    get subscription id from alternative AZURE_SUBSCRIPTION_ID env var

--- apollo/integrations/azure_blob/utils.py
+++ apollo/integrations/azure_blob/utils.py
@@ -34,34 +34,17 @@ class AzureUtils:
             raise AgentConfigurationError("Unable to get function name")
         return function_name
 
-    @classmethod
-    def get_subscription_id(cls) -> str:
+    @staticmethod
+    def get_subscription_id() -> str:
+        if subscription_id := os.getenv("AZURE_SUBSCRIPTION_ID"):
+            return subscription_id
         owner_name = os.getenv(
             "WEBSITE_OWNER_NAME"
         )  # subscription_id+resource_group_region_etc
         if not owner_name:
-            # try getting the subscription id using the default credentials
-            subscription_id = cls._get_subscription_id_from_credentials()
-            if subscription_id:
-                return subscription_id
             raise AgentConfigurationError("Unable to get subscription id")
         if "+" not in owner_name:
             raise AgentConfigurationError(
                 f"Unable to get subscription id, invalid owner name: {owner_name}"
             )
         return owner_name.split("+")[0]
-
-    @staticmethod
-    def _get_subscription_id_from_credentials() -> Optional[str]:
-        # imported here to prevent issues in other platforms
-        from azure.mgmt.resource import SubscriptionClient
-
-        client = SubscriptionClient(AzureUtils.get_default_credential())
-        subscriptions = list(client.subscriptions.list())
-        if not subscriptions:
-            logger.error("No subscriptions found using credentials")
-            return None
-        if len(subscriptions) > 1:
-            logger.error("Multiple subscriptions found using credentials")
-            return None
-        return subscriptions[0].subscription_id
